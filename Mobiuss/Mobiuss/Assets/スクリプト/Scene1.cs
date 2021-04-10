296c7cc9581091bd2a933ee6276838ad6258fd5e
yanai <20cge0020ys@edu.sendai-com.ac.jp>
2021-04-10 18:13:20 +0900

    Mobiuss です。

---
+++ Mobiuss/Mobiuss/Assets/スクリプト/Scene1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Prologue");
+    }
+}
