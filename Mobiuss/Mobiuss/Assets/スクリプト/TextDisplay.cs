44947759cbbeeed32059c6671ee2cb9170b4bc55
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2021-04-15 15:41:22 +0900

    Mobiuss 移行作業

---
+++ Mobiuss/Mobiuss/Assets/スクリプト/TextDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextDisplay : MonoBehaviour
+{
+    //作やすひろ
+    public Text StartText;
+    [SerializeField] GameObject secondText;
+    public int TextTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //DelayMethodを3.5秒後に呼び出す
+        Invoke("DelayMethod", 3.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void DelayMethod()
+    {
+        StartText.gameObject.SetActive(false);
+        secondText.gameObject.SetActive(true);
+        Invoke("DelayMethods", 3.5f); 
+    }
+    void DelayMethods()
+    {
+        secondText.gameObject.SetActive(false);
+        TextTime = 1;
+    }
+}
