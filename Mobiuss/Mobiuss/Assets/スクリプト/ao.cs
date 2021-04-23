0a844f90adcce7a67545c6dbfaadbcb6e6e7d2d4
yanai <20cge0020ys@edu.sendai-com.ac.jp>
2021-04-24 05:51:25 +0900

    stage1を(大体)完成させました。

---
+++ Mobiuss/Mobiuss/Assets/スクリプト/ao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ao : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            i = 1;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Mobiuss/Mobiuss/Assets/スクリプト/kiirobotan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kiirobotan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kiiro.i == 1)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
