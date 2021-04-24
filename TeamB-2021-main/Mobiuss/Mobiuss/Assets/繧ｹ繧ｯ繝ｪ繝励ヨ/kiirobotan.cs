aa66f066296bf91dc1a20ddaf193d3da8d71ab22
OtomoTakeru <20cge0002ot@edu.sendai-com.ac.jp>
2021-04-24 21:50:32 +0900

    0424やない作業切り抜きまとめ

---
+++ TeamB-2021-main/Mobiuss/Mobiuss/Assets/繧ｹ繧ｯ繝ｪ繝励ヨ/kiirobotan.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kiirobotan : MonoBehaviour
+{
+    int j ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        j=0;
+        transform.Rotate(new Vector3(0, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (kiiro.i == 1)
+        {
+           
+            if (j < 900) { 
+                transform.Rotate(new Vector3(0, 0, -0.2f));
+                j+=2;
+            }
+
+        }
+    }
+}
