818ca22420a1eb58bffa20d51e10040c105bdd05
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2021-04-24 10:33:02 +0900

    Revert "Revert "クリア演出とロード画面""
    This reverts commit 85633d8da3f882a9d78dd627b61a228b5de4c0e2.

--- Mobiuss/Mobiuss/Assets/Script/TimeCounter.cs
+++ Mobiuss/Mobiuss/Assets/Script/TimeCounter.cs
@@ -7,7 +7,12 @@ using UnityEngine.SceneManagement;
 public class TimeCounter : MonoBehaviour
 {
     //カウントダウン
-    public float countdown = 5.0f;
+    public float countdown = 300.0f;
+    //取得用関数
+    public float Getcountdown()
+    {
+        return countdown;
+    }
 
     //時間を表示するText型の変数
     public Text timeText;
