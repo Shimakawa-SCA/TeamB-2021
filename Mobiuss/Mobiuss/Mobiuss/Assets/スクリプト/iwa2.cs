853ed3660d9dbc2565c088eb0be5f1d81ac73954
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2021-05-11 20:55:30 +0900

    　　　　  ・ ・ ・ ・ ステージ2、おおわだできました。

---
+++ Mobiuss/Mobiuss/Mobiuss/Assets/スクリプト/iwa2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iwa2 : MonoBehaviour
+{
+    int n = 0;
+    public GameObject iwaPrefab;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    //　出現させるエフェクト
+    [SerializeField]
+    private GameObject effectObject;
+    //　エフェクトを消す秒数
+    [SerializeField]
+    private float deleteTime;
+    //　エフェクトの出現位置のオフセット値
+    [SerializeField]
+    private float offset;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        n=0;
+        aka2.i=0;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "yuka")
+        {
+           
+            var instantiateEffect = GameObject.Instantiate(effectObject, transform.position + new Vector3(0f, offset, 0f), Quaternion.identity) as GameObject;
+            Destroy(instantiateEffect, deleteTime);
+            GameObject iwa = Instantiate(iwaPrefab);
+            iwa.transform.position = new Vector3(7.209985f, 1.822106f, -0.192756f);
+            Destroy(gameObject);
+        } 
+    }
+            // Update is called once per frame
+    void Update()
+    {
+        if (aka2.i == 1)
+        {
+            //n++;
+            //Position.x += 10;
+            rb.velocity = new Vector3(-3f, -3f, 0);
+        }
+
+    }
+}
