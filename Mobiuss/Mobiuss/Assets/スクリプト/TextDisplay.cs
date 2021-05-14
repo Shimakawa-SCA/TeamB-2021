dbf109391a6d2ccd08b2337277a5788ca4f3dbad
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2021-05-14 14:51:00 +0900

    ステージ２見つけました

--- Mobiuss/Mobiuss/Assets/スクリプト/TextDisplay.cs
+++ Mobiuss/Mobiuss/Assets/スクリプト/TextDisplay.cs
@@ -6,16 +6,15 @@ using UnityEngine.UI;
 
 public class TextDisplay : MonoBehaviour
 {
-    //作やすひろ 
+    //作やすひろ
     public Text StartText;
     [SerializeField] GameObject secondText;
     public int TextTime = 0;
-
     // Start is called before the first frame update
     void Start()
     {
         //DelayMethodを3.5秒後に呼び出す
-        Invoke("DelayMethod", 1.5f);
+        Invoke("DelayMethod", 3.5f);
     }
 
     // Update is called once per frame
@@ -24,10 +23,10 @@ public class TextDisplay : MonoBehaviour
         
     }
     void DelayMethod()
-    {    
+    {
         StartText.gameObject.SetActive(false);
         secondText.gameObject.SetActive(true);
-        Invoke("DelayMethods", 2.5f); 
+        Invoke("DelayMethods", 3.5f); 
     }
     void DelayMethods()
     {
---
+++ Mobiuss/Mobiuss/Assets/スクリプト/s2yuka3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s2yuka3 : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "iwa")
+        {
+            Invoke("yuka", 0.5f);
+        }
+    }
+
+
+    // Update is called once per frame
+    void yuka()
+    {
+        rb.velocity = new Vector3(0, -5f, 0);
+    }
+}
