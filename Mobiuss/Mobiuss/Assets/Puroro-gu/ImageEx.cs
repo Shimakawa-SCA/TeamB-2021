1a8fed6b3c36b7a00ab9fe03876891f223fc616f
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2021-05-20 02:20:23 +0900

    タイトル直し

--- Mobiuss/Mobiuss/Assets/Puroro-gu/ImageEx.cs
+++ Mobiuss/Mobiuss/Assets/Puroro-gu/ImageEx.cs
@@ -8,22 +8,53 @@ public class ImageEx : MonoBehaviour
 {
     [SerializeField] private GameObject BookImag;
     [SerializeField] private GameObject BooksImag;
+    [SerializeField] private Button StartButton;
+
 
     // Start is called before the first frame update
     void Start()
     {
         BookImag.SetActive(true);
         BooksImag.SetActive(false);
+        
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            BookImag.SetActive(false);
-            BooksImag.SetActive(true);
+            WhiteOut();
+            
         }
+        
     }
-    
+
+
+
+    void WhiteOut()
+    {
+        //FindObjectOfType<FadeController2>().StartFadeOut();
+        FadeController2.isFadeOut = true;
+        Invoke("Books",1.0f);
+        Invoke("Whitein", 2.0f);
+    }
+    void Books()
+    {
+        BookImag.SetActive(false);
+        BooksImag.SetActive(true);
+    }
+    void Whitein()
+    {
+        FadeController2.isFadeIn = true;
+        TextFead.isFadeIn = true;
+        //Invoke(StartButtin,2.0f);
+        //FindObjectOfType<TextFead>().FadeIn();
+    }
+    /*void StartButtin()
+    {
+        StartButton.SetActive(true);
+    }*/
 }
