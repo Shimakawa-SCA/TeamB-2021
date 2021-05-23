0c6f4ec5722dfffa4c2544100b52c3784d561fd7
OowadaYasuhiro <20cge0003oy@edu.sendai-com.ac.jp>
2021-05-23 13:29:26 +0900

    貰ったもの入れました

---
+++ Mobiuss/Mobiuss/Assets/スクリプト/NextScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ImageEx.NextSceneCount == 1 && Input.GetKeyDown("joystick button 0"))
+        {
+            FadeController.isFadeOut = true;
+            Invoke("next",2.0f);
+        }
+    }
+
+    void next()
+    {
+        SceneManager.LoadScene("SampleScene"); 
+    }
+}
