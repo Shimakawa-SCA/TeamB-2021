a043833179e06e75558988fa67f5a01841348a7a
401-03\cre <test>
2021-06-12 14:51:41 +0900

    aaaa

---
+++ Mobiuss/takesanPlayer/Assets/NewTakesanPlayer/Script/NewTakesanPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTakesanPlayer : MonoBehaviour
+{
+    SpriteRenderer playersprite;
+    public Sprite waitright1;
+    public Sprite waitleft1;
+    bool a = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playersprite = GetComponent<SpriteRenderer>();
+        Debug.Log(playersprite.sprite);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) a = !a;
+        if (a) playersprite.sprite = waitleft1;
+        if (!a) playersprite.sprite = waitright1;
+    }
+}
