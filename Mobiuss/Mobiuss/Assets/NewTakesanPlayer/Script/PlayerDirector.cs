4a0e9978988c76a4ac866277ad09602f09c47cac
2PC-35\student <20cge0002ot@edu.sendai-com.ac.jp>
2021-06-19 17:15:57 +0900

    プレイヤーいれた

---
+++ Mobiuss/Mobiuss/Assets/NewTakesanPlayer/Script/PlayerDirector.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//タイミング200
+public class PlayerDirector : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject cadaver;
+    public GameObject cadaverl;
+    public GameObject cadaver3;
+    public GameObject cadaverl3;
+    int RespawnCount;
+    public Vector3[] SpawnPoint;
+    bool RespawnStack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RespawnCount = 0;
+        RespawnStack = false;
+        PassInitialize();
+        SetPass();
+        Invoke("FirstReSpawn",0.1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((RespawnStack == false) && (Input.GetKeyDown(KeyCode.R))){
+            Invoke("PlayerSpawn",1f);
+            Invoke("InsCav",0.9f);
+        }
+        SetPass();
+    }
+
+    void FirstReSpawn(){
+        RespawnStack = true;
+        Instantiate(Player, SpawnPoint[Pass.StageNumber], Quaternion.identity);
+        Invoke("SpawnDirector", 0.3f);
+    }
+
+    void PassInitialize(){
+        Pass.RespawnPoint = RespawnCount;
+        Pass.RespawnStack = RespawnStack;
+    }
+
+    void SetPass(){
+        Pass.RespawnPoint = RespawnCount;
+        Pass.RespawnStack = RespawnStack;
+    }
+
+    void InsCav(){
+
+    }
+
+    void PlayerSpawn(){
+        RespawnStack = true;
+        if (Pass.StageNumber == 1 || Pass.StageNumber == 2){
+            if (Pass.PlayerRight == true){
+                Instantiate(cadaver,Pass.PlayerPosition,Quaternion.identity);
+            }
+            if (Pass.PlayerRight == false){
+                Instantiate(cadaverl, Pass.PlayerPosition, Quaternion.identity);
+            }
+        }
+        if (Pass.StageNumber == 3){
+            if (Pass.PlayerRight == true){
+                Instantiate(cadaver3, Pass.PlayerPosition, Quaternion.identity);
+            }
+            if (Pass.PlayerRight == true){
+                Instantiate(cadaverl3, Pass.PlayerPosition, Quaternion.identity);
+            }
+        }
+        Instantiate(Player,SpawnPoint[Pass.StageNumber],Quaternion.identity);
+        Invoke("SpawnDirector",0.3f);
+        RespawnCount++;
+    }
+
+    void SpawnDirector(){
+        RespawnStack = false;
+    }
+}
