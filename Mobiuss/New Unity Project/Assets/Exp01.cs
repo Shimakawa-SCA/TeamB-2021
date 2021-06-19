d0ddf865e15ee9593f7bffc4753e7210b0e8d515
401-03\cre <test>
2021-06-19 09:57:57 +0900

    かんけいなし

---
+++ Mobiuss/New Unity Project/Assets/Exp01.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp01 : MonoBehaviour
+{
+    int Lv,Exp;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Lv = 1;
+       Exp = 125;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Mobiuss/New Unity Project/Assets/Exp02.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp02 : MonoBehaviour
+{
+    int Lv,Exp;
+    // Start is called before the first frame update
+    void Start()
+    {
+      Lv = 2;
+      Exp = 250;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
