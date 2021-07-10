14826aa60a30108e60d3c158e1fb78788e4eae03
401-03\cre <test>
2021-07-10 11:13:23 +0900

    かんけいなし

---
+++ Mobiuss/New Unity Project (1)/Assets/NewBehaviourScript3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 v1 = new Vector3(1,2,3);
+        Vector3 v2 = new Vector3(4,5,6);
+        Debug.Log(InnerProduct(v1,v2));
+    }
+
+    float InnerProduct(Vector3 vec1, Vector3 vec2)
+    {
+        float calc =0;
+        calc=vec1.x*vec2.x
+            +vec1.y*vec2.y
+            +vec1.z*vec2.z;
+
+    }
+   
+}
---
+++ Mobiuss/New Unity Project (1)/Assets/NewBehaviourScript4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript4 : MonoBehaviour
+{
+    float sum=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Mathf.PI);
+        for(int n=0; n<100; n++)
+        {
+            sum +=4*(Mathf.Pow(-1,n) / (2*n+1));
+        }
+        Debug.Log(sum);
+    }
+
+    
+}
